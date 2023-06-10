aedad4a240b1a1437ec3ccaa63d92e321560c504
Suat Özgür <s.oezguer@gmail.com>
2023-06-10 02:36:24 +0200

    update TRIM

--- static/trim.py
+++ static/trim.py
@@ -1,13 +1,31 @@
 import bpy
 
-bpy.data.objects["OriginalText"].data.body = "###TEXT###"
-bpy.data.objects["OriginalText"].select_set(True)
-bpy.context.view_layer.objects.active = bpy.data.objects['OriginalText']
+objTrim = bpy.data.objects["Trim"];
+objText = bpy.data.objects["Text"];
+objDifference = bpy.data.objects["Difference"];
+objIntersect = bpy.data.objects["Intersect"];
+
+
+objText.data.body = "###TEXT###"
+
+objTextMesh = objText.copy()
+objTextMesh.data = objText.data.copy()
+bpy.context.collection.objects.link(objTextMesh)
+
+bpy.ops.object.select_all(action='DESELECT')
+objTextMesh.select_set(True)
+bpy.context.view_layer.objects.active = objTextMesh
 bpy.ops.object.convert(target="MESH")
 
-bpy.data.objects["OriginalText"].name = "DifferenceObject"
+boolDiff = objTextMesh.modifiers.new(type="BOOLEAN", name="bool 1")
+boolDiff.object = objDifference
+boolDiff.operation = 'DIFFERENCE'
+
+boolIntersect = objTextMesh.modifiers.new(type="BOOLEAN", name="bool 2")
+boolIntersect.object = objIntersect
+boolIntersect.operation = 'INTERSECT'
+
+objTrim.select_set(True)
 
 
-bpy.data.objects["Ring.001"].select_set(True)
-bpy.context.view_layer.objects.active = bpy.data.objects['Ring.001']
 bpy.ops.export_mesh.stl(filepath=str(('/media/files/###FILE###.stl')),   global_scale=10, use_selection=True)
